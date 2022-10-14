a4cc0775c27ff8bd1781e7e56b1479601d072521
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-10-14 16:17:14 +0600

    user serializers fix

--- users/serializers.py
+++ users/serializers.py
@@ -6,12 +6,13 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'password')
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
         extra_kwargs = {
             'password': {'required': True, 'write_only': True}
         }
 
     def create(self, validated_data):
+        # Todo: add confirm password validation
         user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
